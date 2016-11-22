700d735b4f4d41e0d26fa9c1752492d735caf1e0
Michael Montgomery <montgomery.michael@hotmail.com>
2016-11-22 00:57:52 -0500

    content and styling on home page

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -8,5 +8,13 @@ app.config(function($routeProvider) {
 });
 
 app.controller('welcomeController', function($scope) {
-
+    $(window).scroll(function() {
+        if($(window).scrollTop() === 0) {
+            $('.welcome-header').css('background-color', 'black');
+            $('.welcome-header nav a').css('color', 'white');
+        } else {
+            $('.welcome-header').css('background-color', 'white');
+            $('.welcome-header nav a').css('color', 'black')
+        }
+    })
 })
